0402190b64575847494b0b50efe503b0f8762e7b
Tournier Maxime <51545381+Hoggy06@users.noreply.github.com>
2021-11-06 18:52:06 +0100

    Affichage des éléments propre au produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,36 @@
 
 const search_params = new URLSearchParams(window.location.search); 
   const id = search_params.get('id');
-  console.log(id)
+
+  fetch(`http://localhost:3000/api/products/${id}`)
+
+.then((response) => {
+    return response.json();
+})
+ 
+.then((data) => {
+
+  class kanap {
+    constructor(colors, name, price, description, imageUrl, altTxt) {
+      this.colors = data.colors;
+      this.name = data.name;
+      this.price = data.price;
+      this.description = data.description;
+      this.imageUrl = data.imageUrl;
+      this.altTxt = data.altTxt;
+    }
+    kanapProduct() {
+      document.getElementById("title").innerHTML += this.name;
+      document.getElementById("price").innerHTML += this.price;
+      document.getElementById("description").innerHTML += this.description;
+      document.getElementsByClassName("item__img").innerHTML += `<img src="${this.imageUrl}" alt="${this.altTxt}">`;
+      document.getElementById("colors").innerHTML += this.colors;
+    }
+  }
+  const k = new kanap();
+  k.kanapProduct();
+})
+
+.catch((error) => {
+    console.log(error)
+});
