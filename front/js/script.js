3ebf798602b4d8f90361c0ddd9cb1430b76d9c31
Tournier Maxime <51545381+Hoggy06@users.noreply.github.com>
2021-11-06 23:09:29 +0100

    Modification

--- front/js/script.js
+++ front/js/script.js
@@ -9,10 +9,8 @@ fetch("http://localhost:3000/api/products")
 .then((data) => {
     for(let d in data) {
 
-        const url = new URL(`http://127.0.0.1:5500/front/html/product.html?id=${data[d]._id}`);
-
         const htmlContent = `
-            <a href="${url}">
+            <a href="./product.html?id=${data[d]._id}">
                 <article>
                     <img src="${data[d].imageUrl}" alt="${data[d].altTxt}">
                     <h3 class="productName">${data[d].name}</h3>
