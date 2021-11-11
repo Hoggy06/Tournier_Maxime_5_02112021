e475ca7fe9f1f1d269ca12a6c67ecf813cbc6aa8
Tournier Maxime <51545381+Hoggy06@users.noreply.github.com>
2021-11-11 18:29:44 +0100

    Ajout de fichier pas indispensable au repo

--- front/js/product.js
+++ front/js/product.js
@@ -1,53 +1,55 @@
-
-const search_params = new URLSearchParams(window.location.search); 
-const id = search_params.get('id');
+const search_params = new URLSearchParams(window.location.search);
+const id = search_params.get("id");
 
 fetch(`http://localhost:3000/api/products/${id}`)
+  .then((response) => {
+    return response.json();
+  })
 
-.then((response) => {
-  return response.json();
-})
- 
-.then((data) => {
+  .then((data) => {
+    class kanap {
+      constructor(id, colors, name, price, description, imageUrl, altTxt) {
+        this.id = data._id;
+        this.colors = data.colors;
+        this.name = data.name;
+        this.price = data.price;
+        this.description = data.description;
+        this.imageUrl = data.imageUrl;
+        this.altTxt = data.altTxt;
+      }
 
-  class kanap {
-    constructor(id, colors, name, price, description, imageUrl, altTxt) {
-      this.id = data._id;
-      this.colors = data.colors;
-      this.name = data.name;
-      this.price = data.price;
-      this.description = data.description;
-      this.imageUrl = data.imageUrl;
-      this.altTxt = data.altTxt;
-    }
-  
-    
+      kanapProduct() {
+        document.getElementById("title").innerHTML += this.name;
+        document.getElementById("price").innerHTML += this.price;
+        document.getElementById("description").innerHTML += this.description;
 
-    kanapProduct() {
+        const img = document.createElement("img");
+        img.setAttribute("src", this.imageUrl);
+        img.setAttribute("alt", this.altTxt);
 
-      document.getElementById("title").innerHTML += this.name;
-      document.getElementById("price").innerHTML += this.price;
-      document.getElementById("description").innerHTML += this.description;
+        document.getElementsByClassName("item__img")[0].appendChild(img);
 
-      const img = document.createElement('img');
-      img.setAttribute("src", this.imageUrl);
-      img.setAttribute("alt", this.altTxt);
-
-      document.getElementsByClassName("item__img")[0].appendChild(img);
-      
-      for(let c of data.colors) {
-        document.getElementById("colors").innerHTML += `<option value="${c}">${c}</option>`;
+        for (let c of data.colors) {
+          document.getElementById("colors").innerHTML += `<option value="${c}">${c}</option>`;
+        
+          //document.getElementsByTagName("option")[c].classList.add("colorsOptionValue");
+          
+        }
       }
     }
+    const k = new kanap();
+    k.kanapProduct();
 
-   
-  }
+    const addToCart = document.getElementById("addToCart");
 
-  const k = new kanap();
-  k.kanapProduct();
+    addToCart.addEventListener("click", function(e){
+      e.preventDefault();
+      const arrayProduct = new kanap();
 
-})
 
-.catch((error) => {
-    console.log(error)
-});
+    })
+  })
+
+  .catch((error) => {
+    console.log(error);
+  });
--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,14 @@
-
+import "./product.js"
+import "./cart.js"
 
 fetch("http://localhost:3000/api/products")
-
-.then((response) => {
+  .then((response) => {
     return response.json();
-})
- 
-.then((data) => {
+  })
 
-    for(let d in data) {
-        const htmlContent = `
+  .then((data) => {
+    for (let d in data) {
+      const htmlContent = `
             <a href="./product.html?id=${data[d]._id}">
                 <article>
                     <img src="${data[d].imageUrl}" alt="${data[d].altTxt}">
@@ -17,11 +16,25 @@ fetch("http://localhost:3000/api/products")
                     <p class="productDescription">${data[d].description}</p>
                 </article>
             </a>`;
-    document.getElementById("items").innerHTML += htmlContent;
+      document.getElementById("items").innerHTML += htmlContent;
     }
-    
-})
+  })
 
-.catch((error) => {
-    console.log(error)
-});
+  .catch((error) => {
+    const errorAlert = () => {
+      const articleError = document.createElement("article");
+      const messageError = document.createElement("p");
+
+      articleError.classList.add("articleError");
+      messageError.classList.add("messageError");
+
+      articleError.style.backgroundColor = "#AD1B1B";
+      messageError.style.color = "white";
+      messageError.style.fontWeight = "bold";
+
+      document.getElementById("items").appendChild(articleError);
+
+      return (articleError.appendChild(messageError).innerHTML += error);
+    };
+    errorAlert();
+  });
