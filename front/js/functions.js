82144d0f98de9e28cacc914601605447891b141b
Max <tourniermaxime@orange.fr>
2021-11-13 21:50:51 +0100

    Ajout de nouvelles fonctions price et totalQuantity

--- front/js/functions.js
+++ front/js/functions.js
@@ -1,28 +1,18 @@
-export default class kanap {
-    constructor(id, colors, name, price, description, imageUrl, altTxt) {
-      this.id = data._id;
-      this.colors = data.colors;
-      this.name = data.name;
-      this.price = data.price;
-      this.description = data.description;
-      this.imageUrl = data.imageUrl;
-      this.altTxt = data.altTxt;
-    }
+import { cart } from "./cart.js";
 
-    kanapProduct() {
-      document.getElementById("title").innerHTML += this.name;
-      document.getElementById("price").innerHTML += this.price;
-      document.getElementById("description").innerHTML += this.description;
-
-      const img = document.createElement("img");
-      img.setAttribute("src", this.imageUrl);
-      img.setAttribute("alt", this.altTxt);
-
-      document.getElementsByClassName("item__img")[0].appendChild(img);
-
-      for (let c in this.colors) {
-        document.getElementById("colors").innerHTML += `<option value="${this.colors[c]}">${this.colors[c]}</option>`;
-        
-      }
-  }
+export function price() {
+  let total = 0;
+  cart.forEach((obj) => {
+      total = total + obj.price * obj.quantity;
+  });
+  return total;
 }
+
+export function totalQuantity() {
+  let totalQuantity = 0;
+  cart.forEach((obj) => {
+    totalQuantity = totalQuantity + obj.quantity;
+  });
+  return totalQuantity;
+}
+
