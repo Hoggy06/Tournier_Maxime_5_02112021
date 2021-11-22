3a52e564bf632f74aa8e4be785769bbc32d429d2
Max <tourniermaxime@orange.fr>
2021-11-22 21:59:08 +0100

    Finalisation de la page avec envoie du formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,6 @@
 import { totalPrice, totalQuantity, changeQuantity, deleteItem } from "./functions.js"; 
 
 export const cart = JSON.parse(localStorage.getItem("cart")); // On récupère le localStorage utilisé dans la page produit
-console.log(cart);
 
 for (let c of cart) { // On parcours le tableau et on affiche les données
 
@@ -30,8 +29,6 @@ for (let c of cart) { // On parcours le tableau et on affiche les données
 
   document.getElementById("cart__items").innerHTML += htmlContent;
 
-
-
 }
 
 // Import de la fonction changeQuantity
@@ -45,6 +42,117 @@ document.getElementById("totalQuantity").innerHTML += totalQuantity();
 
 // Affiche le prix totale
 document.getElementById("totalPrice").innerHTML += totalPrice();
+
+function form() {
+
+  const order = document.getElementById("order");
+  order.addEventListener("click", function(e) {
+    e.preventDefault();
+  
+  // Création de l'objet contact
+  const contact = {
+    fisrtName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    address: document.getElementById("address").value,
+    city: document.getElementById("city").value,
+    email: document.getElementById("email").value
+  }
+
+  function testFirstName() {
+    const validFisrtName = contact.fisrtName;
+    if(/^[A-Za-z]{3,20}$/.test(validFisrtName)) {
+      return true;
+    } else {
+      let firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+      firstNameErrorMsg.textContent = "Merci de vérifier votre prénom, 3 caractères minimum requis avec des lettres uniquement";
+    }
+  }
+
+  function testLastName() {
+    const validLastName = contact.lastName;
+    if(/^[A-Za-z]{3,20}$/.test(validLastName)) {
+      return true;
+    } else {
+      let lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
+      lastNameErrorMsg.textContent = "Merci de vérifier votre nom, 3 caractères minimum requis avec des lettres uniquement";
+    }
+  }
+
+  function testAddress() {
+    const validAddress = contact.address;
+    if(/^[a-zA-Z0-9-\s]+$/.test(validAddress)) {
+      return true;
+    } else {
+      let addressErrorMsg = document.getElementById("addressErrorMsg");
+      addressErrorMsg.textContent = "Merci de vérifier votre addresse, caractères alphanumériques autorisés";
+    }
+  }
+
+  function testCity() {
+    const validCity = contact.city;
+    if(/^[a-zA-Z0-9-\s]+$/.test(validCity)) {
+      return true;
+    } else {
+      let cityErrorMsg = document.getElementById("cityErrorMsg");
+      cityErrorMsg.textContent = "Merci de vérifier votre ville, 3 caractères minimum requis avec des lettres uniquement";
+    }
+  }
+
+  function testEmail() {
+    const validEmail = contact.email;
+    if(/^([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/) {
+      return true;
+    } else {
+      let emailErrorMsg = document.getElementById("emailErrorMsg");
+      emailErrorMsg.textContent = "Erreur email non valide !";
+    }
+  }
+
+  function validForm() {
+    if(testFirstName() && testLastName() && testAddress() && testCity() && testEmail()) {
+      localStorage.setItem("contact", JSON.stringify(contact));
+      return true;
+    } else {
+      alert('Merci de vérifier le contenu de votre formulaire');
+    }
+  }
+
+validForm();
+
+let send = {
+  contact,
+  cart
+}
+
+fetch("http://localhost:3000/api/products/order", {
+  method: "POST",
+  body: JSON.stringify(send),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+.then((response) => {
+  return response.json()
+})
+
+.then((data) => {
+  console.log(data);
+  localStorage.setItem("orderId", data.orderId);
+  if(validForm()) {
+    window.location.href = `confirmation.html?id=${data.orderId}`;
+  }
+})
+
+.catch((error) => {
+  alert(error);
+});
+
+})
+
+}
+
+form();
 
 
 
