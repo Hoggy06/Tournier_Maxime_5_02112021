2e2701eae41db1f518211b5e2c71c64ad227f9b5
Max <tourniermaxime@orange.fr>
2021-12-02 21:11:40 +0100

    Indentation + commentaires

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,7 +1,8 @@
-function checkout(){
-    // On récupère l'id passé en paramètre d'URL
-    const search_params = new URLSearchParams(window.location.search); 
-    const orderId = search_params.get("orderId");
-    document.getElementById("orderId").innerHTML += orderId;
+// Fonction qui retourne l'id de la commande
+function checkout() {
+  // On récupère l'id passé en paramètre d'URL
+  const search_params = new URLSearchParams(window.location.search);
+  const orderId = search_params.get("orderId");
+  document.getElementById("orderId").innerHTML += orderId;
 }
 checkout();
